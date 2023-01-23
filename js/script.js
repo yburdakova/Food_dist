952e37400b6d83c8c3c4541a200934bb77a3d2f0
Yana Burdakova <bu.asti@gmail.com>
2023-01-23 13:22:41 -0500

    slider finished

--- js/script.js
+++ js/script.js
@@ -283,7 +283,50 @@ function showThanksModal(message) {
         closeModal();
     }, 4000);
 }
-});
 
 //===========Slider===========================================
+
+const sliders = document.querySelectorAll('.offer__slide'),
+    prev = document.querySelector('.offer__slider-prev'),
+    next = document.querySelector('.offer__slider-next'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current');
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    if (sliders.length < 10){
+        total.textContent = `0${sliders.length}`;
+    } else {
+        total.textContent = sliders.length;
+    }
+
+    function showSlides(n) {
+        if (n > sliders.length) {
+            slideIndex = 1;
+        } 
+        if (n < 1) {
+            slideIndex = sliders.length;
+        }
+
+        sliders.forEach(item => item.style.display = "none");
+        sliders[slideIndex-1].style.display = "block";
+        if (sliders.length < 10){
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    
+    }
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', () => {
+        plusSlides(+1);
+    });
+   
+});
 
